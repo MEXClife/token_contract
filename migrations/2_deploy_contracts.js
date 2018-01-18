fb9cc3bea88eab0128f12feca6885955f2ec3280
Hisham Ismail <mhishami@gmail.com>
2018-01-19 00:07:53 +0800

    Updated deploy script for new args

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,15 +4,39 @@ var EMXToken = artifacts.require("./EMXToken.sol");
 
 module.exports = function(deployer, network, accounts) {
 
-  const preSaleStartTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 10;
-  const preSaleDays = 15;
-  const mainSaleDays = 45;
-  const wallet = accounts[0];
-
+  // deploy EMX Token first.
   deployer.deploy(EMXToken, {});
 
-  deployer.deploy(EMXCrowdsale,                             // the contract
-    preSaleStartTime, preSaleDays, mainSaleDays, wallet,    // the args
-    {});  // extra args
+  // function signature.
+  // EMXCrowdsale (uint256 _preSaleStartTime, uint8 _preSaleDays,
+  //  uint8 _mainSaleDays, address _wallet)
+  // const preSaleStartTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 10;
+  // const preSaleDays = 30;
+  // const mainSaleDays = 45;
+  // const wallet = accounts[0];
+
+  // deployer.deploy(EMXCrowdsale,                             // the contract
+  //   preSaleStartTime, preSaleDays, mainSaleDays, wallet,    // the args
+  //   {});  // extra args
+
+  // EMXCrowdsale (uint256 _startTime, uint256 _endTime, 
+  //                          uint256 _rate, address _wallet,
+  //                          uint256 _cap) 
+  const startPriv = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 10;
+  const endPriv = startPriv + (15 * 86400);
+  const startPre = endPriv + 1;
+  const endPre = endPriv + (30 * 86400);
+  const startPub = endPre + 1;
+  const endPub = endPre + (30 * 86400);
+  const rate = 3500;
+  const wallet = accounts[0];
+  const cap = web3.toWei(200000, 'ether');
+
+  deployer.deploy(EMXCrowdsale,
+                  startPriv, endPriv,
+                  startPre, endPre,
+                  startPub, endPub, 
+                  rate, wallet, cap, 
+                  {});
 
 };
