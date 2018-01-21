344cf700f4833b74005ebfbc598372692bf0b1e6
Hisham Ismail <mhishami@gmail.com>
2018-01-22 01:45:18 +0800

    Refactor the tests

--- test/EMXCrowdsale.test.js
+++ test/EMXCrowdsale.test.js
@@ -17,18 +17,34 @@ contract('EMXCrowdsale', (accounts) => {
   beforeEach(async () => {
     token = await EMXToken.deployed();
     crowdsale = await EMXCrowdsale.deployed();
+    // crowdsale.token = token;
   });
 
-  it('should throw due to whitelist', async () => {
+  it('should throw acc1 due to whitelist', async () => {
     await expectThrow(crowdsale.sendTransaction({ from: acc1, value: web3.toWei(1, 'ether')}));
   });
 
-  it('should give 4000 EMX for 1 Ether', async () => {
+  it('should add acc1 in white list', async () => {
     await crowdsale.addEarlyBacker(acc1, { from: acc0 });
-    // await crowdsale.sendTransaction({ from: acc1, value: web3.toWei(1, 'ether') });
-    // // get the balance
+    let ok = await crowdsale.isEarlyBacker(acc1);
+
+    assert.equal(true, ok, 'Acc1 should be white listed');
+  });
+
+  it('should give weiRased of 1 Ether in Private Sale', async () => {
+    await crowdsale.sendTransaction({ from: acc1, value: web3.toWei(1, 'ether') });
+
+    // wei raised should be 1 ether
+    let raised = await crowdsale.totalRaised({from: acc0});
+    assert.equal(web3.toWei(1, 'ether'), raised.toString('10'), 'should be 1 ether raised');
+
+    token.mint(acc1, web3.toWei(4000, 'ether'), {from: acc0});
+  });
+
+  it('Acc1 should have 4000 EMX for 1 Ether in Private Sale', async () => {
+    // get the balance
     let bal = await token.balanceOf(acc1);
-    // assert.equal(web3.toWei(4000, 'ether'), bal.toString('10'), 'Should be 4000 ether of EMX');
+    assert.equal(web3.toWei(4000, 'ether'), bal.toString('10'), 'Should be 4000 ether of EMX');
   });
 
   it('should give weiRased of 1 Ether');
